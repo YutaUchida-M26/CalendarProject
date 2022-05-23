343cca2c830c84545d243cbfafd4af166a0e4ceb
YutaUchida-M26 <s1260087@gmail.com>
2022-05-24 00:04:47 +0900

    Calendar mixed

--- Calapp/views.py
+++ Calapp/views.py
@@ -2,6 +2,10 @@ from django.views import generic
 from . import mixins
 from .models import Schedule
 
+import datetime
+from django.shortcuts import redirect
+from .forms import BS4ScheduleForm
+
 
 class MonthCalendar(mixins.MonthCalendarMixin, generic.TemplateView):
     """月間カレンダーを表示するビュー"""
@@ -51,3 +55,31 @@ class MonthWithScheduleCalendar(mixins.MonthWithScheduleMixin, generic.TemplateV
         calendar_context = self.get_month_calendar()
         context.update(calendar_context)
         return context
+
+class MyCalendar(mixins.MonthCalendarMixin, mixins.WeekWithScheduleMixin, generic.CreateView):
+    """月間カレンダー、週間カレンダー、スケジュール登録画面のある欲張りビュー"""
+    template_name = 'mycalendar.html'
+    model = Schedule
+    date_field = 'date'
+    form_class = BS4ScheduleForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        week_calendar_context = self.get_week_calendar()
+        month_calendar_context = self.get_month_calendar()
+        context.update(week_calendar_context)
+        context.update(month_calendar_context)
+        return context
+
+    def form_valid(self, form):
+        month = self.kwargs.get('month')
+        year = self.kwargs.get('year')
+        day = self.kwargs.get('day')
+        if month and year and day:
+            date = datetime.date(year=int(year), month=int(month), day=int(day))
+        else:
+            date = datetime.date.today()
+        schedule = form.save(commit=False)
+        schedule.date = date
+        schedule.save()
+        return redirect('Calapp:mycalendar', year=date.year, month=date.month, day=date.day)
